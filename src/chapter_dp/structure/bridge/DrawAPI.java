2d4c65a075d3f7aea53218d25397c221ebf67f1e
yuanhao <994303>
2018-06-04 20:53:03 +0800

    结构型模式-桥接模式：通过提供抽象化和实现化之间的桥接结构，来实现二者的解耦

---
+++ src/chapter_dp/structure/bridge/DrawAPI.java
@@ -0,0 +1,12 @@
+package chapter_dp.structure.bridge;
+
+/**
+ * 创建桥接实现接口。
+ * @author yuanhao
+ * @date 2018/6/4 20:40
+ */
+public interface DrawAPI {
+
+    public void drawCircle(int radius, int x, int y);
+
+}
