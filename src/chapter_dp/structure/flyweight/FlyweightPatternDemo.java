2115a94c20398dab752d087e1bb7feeb835323ef
yuanhao <994303>
2018-06-08 15:10:31 +0800

    结构型模式-享元模式：用于减少创建对象的数量，以减少内存占用和提高性能

---
+++ src/chapter_dp/structure/flyweight/FlyweightPatternDemo.java
@@ -0,0 +1,33 @@
+package chapter_dp.structure.flyweight;
+
+/**
+ * @author yuanhao
+ * @date 2018/6/8 15:02
+ */
+public class FlyweightPatternDemo {
+
+    private static final String colors[] = {"Red", "Green", "Blue", "White", "Black"};
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 20; i++) {
+            Circle circle = (Circle) ShapeFactory.getCircle(getRandomColor());
+            circle.setX(getRandomX());
+            circle.setY(getRandomY());
+            circle.setRadius(100);
+            circle.draw();
+        }
+    }
+
+    public static String getRandomColor() {
+        return colors[(int)(Math.random()*colors.length)];
+    }
+
+    public static int getRandomX() {
+        return (int)(Math.random()*100);
+    }
+
+    public static int getRandomY() {
+        return (int)(Math.random()*100);
+    }
+
+}
---
+++ src/chapter_dp/structure/flyweight/Shape.java
@@ -0,0 +1,12 @@
+package chapter_dp.structure.flyweight;
+
+/**
+ * 创建一个接口。
+ * @author yuanhao
+ * @date 2018/6/8 14:56
+ */
+public interface Shape {
+
+    void draw();
+
+}
