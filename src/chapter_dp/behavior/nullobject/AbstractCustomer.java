c01735d215afb5a69f64133c12d75f85f8ce1500
郑元浩 <zhengyuanhao@guazi.com>
2018-06-18 21:04:01 +0800

    行为型模式-空对象模式：一个空对象取代NULL对象实例的检查。

---
+++ src/chapter_dp/behavior/nullobject/AbstractCustomer.java
@@ -0,0 +1,13 @@
+package chapter_dp.behavior.nullobject;
+
+/**
+ * 创建一个抽象类。
+ * Created by 18710 on 2018/6/18.
+ */
+public abstract class AbstractCustomer {
+
+    protected String name;
+    public abstract boolean isNil();
+    public abstract String getName();
+
+}
